1cb167cc3ecd061a27a4379e2316a8011e9cd44e
Jose <neuralboot19@gmail.com>
2020-04-02 10:23:12 -0500

    GRUD user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,55 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render status: 200, json: { users: @users.all.as_json }
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render status: 201, json: @user
+    else
+      render status: 400, json: @user.errors
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render status: 400, json: @user.errors
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    if @user.destroy
+      render status: 200
+    else
+      render status: 400
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:username)
+    end
+end
